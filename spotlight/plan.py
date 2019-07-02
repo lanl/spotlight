e668b047cbde8269cb472148214ca55b54f732ef
Christopher M. Biwer <cmbiwer@lanl.gov>
2019-07-02 10:57:43 -0600

    Docs.

--- spotlight/plan.py
+++ spotlight/plan.py
@@ -9,45 +9,31 @@ class BasePlan(models.AbstractFunction):
 
     Attributes
     ----------
-    config_file : str
-        Path to concatenated configuration file.
-    data_file : str
-        Path to experimental data file.
+    idxs : dict
+        A ``dict`` with key parameter and value index of parameter.
     bounds : dict
         A ``dict`` with key parameter name and value a tuple of lower and upper
         bounds.
-    names : list
-        A list of names. This list is ordered how packages will return a list.
-    idxs : dict
-        A ``dict`` with key parameter and value index of parameter.
-    det : Detector
+    data_file : str
+        Path to experimental data file.
+    detector : Detector
         A ``Dectector`` instance.
     phases : list
         A list of ``Phase`` instances.
-    lower_bounds : list
-        A list of lower bound values. This list is ordered how packages will
-        return a list.
-    upper_bounds : list
-        A list of upper bound values. This list is ordered how packages will
-        return a list.
 
     Parameters
     ----------
-    config_files : list
-        Paths to configuration files.
-    refinement_plan_file : str
-        Path to refinement plan file.
+    idxs : dict
+        A ``dict`` with key parameter and value index of parameter.
+    bounds : dict
+        A ``dict`` with key parameter name and value a tuple of lower and upper
+        bounds.
     data_file : str
         Path to experimental data file.
-    tmp_dir : str
-        Temporary directory to do refinement.
-    names : {None, list}
-        A list of parameter names in preferred ordered.
-    change : bool
-        Change into temporary directory. Default is ``True``.
-    config_overrides : {None, list}
-        A list of `str` delimited by colons to add options to the configuration
-        file. The format is "section:option:value".
+    detector : Detector
+        A ``Dectector`` instance.
+    phases : list
+        A list of ``Phase`` instances.
     """
 
     def __init__(self, idxs, bounds, data_file, detector, phases, **kwargs):
@@ -61,7 +47,6 @@ class BasePlan(models.AbstractFunction):
         self.data_file = data_file
         self.detector = detector
         self.phases = phases
-        self.num_phases = len(self.phases)
 
         # setup initial porition of refinement plan
         self.initialize()
