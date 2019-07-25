de989fbcfefa01483371ec7c3b74c77ca9bf7b26
Christopher M. Biwer <cmbiwer@lanl.gov>
2019-07-25 08:47:25 -0600

    Formatting.

--- examples/alumina/inspect_solution.py
+++ examples/alumina/inspect_solution.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import numpy
 from klepto import archives
 
 input_file = "tmp_spotlight/solution_alumina.pkl"
@@ -9,7 +10,17 @@ arch.load()
 keys = arch.keys()
 print(keys)
 for key in keys:
-    print(key, arch[key])
+    if key == "names":
+        continue
+    sol = arch[key]
+    print("The key is", key)
+    print("The shape of the parameters array is", numpy.array(sol[0]).shape)
+    print("The shape of the solution array is", numpy.array(sol[1]).shape)
+    print("The best parameters are:")
+    for i, x in enumerate(sol[2]):
+        print(arch["names"][i], x)
+    print("The best cost function value is", sol[3])
+    print()
 
 #from spotlight import archive
 #archive_file = "tmp_spotlight/solution_alumina.pkl"
