f0ee4352e57d454ac55bb3149ae2fbdce07aa450
Christopher M. Biwer <cmbiwer@lanl.gov>
2019-07-25 09:08:45 -0600

    More info.

--- examples/alumina/inspect_solution.py
+++ examples/alumina/inspect_solution.py
@@ -3,12 +3,16 @@
 import numpy
 from klepto import archives
 
+# read solutions
 input_file = "tmp_spotlight/solution_alumina.pkl"
 arch = archives.file_archive(input_file)
 arch.load()
 
+# print information about each local solver
 keys = arch.keys()
 print(keys)
+nsolvers = 0
+nsolvers_terminated = 0
 for key in keys:
     if key == "names":
         continue
@@ -20,7 +24,13 @@ for key in keys:
     for i, x in enumerate(sol[2]):
         print(arch["names"][i], x)
     print("The best cost function value is", sol[3])
+    print("The state of this local optimization is", sol[4])
     print()
+    nsolvers += 1
+    nsolvers_terminated += 1 if sol[4] == None else 0
+
+# print aggregate information about ensemble of local solvers
+print("{} of {} local solvers have terminated".format(nsolvers_terminated, nsolvers))
 
 #from spotlight import archive
 #archive_file = "tmp_spotlight/solution_alumina.pkl"
