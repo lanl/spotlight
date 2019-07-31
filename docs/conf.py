d039df870b40c634b5f308cb23d47d434d7669fe
Christopher M. Biwer <cmbiwer@lanl.gov>
2019-07-31 16:20:56 -0600

    Change sidebar.

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,7 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+html_sidebars = {'**': ['globaltoc.html', 'relations.html']}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
