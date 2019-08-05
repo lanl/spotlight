fde22ffff3d05d09f14771f126efd6d3874f88e4
Christopher Michael Biwer <cmbiwer@lanl.gov>
2019-08-05 17:20:47 -0600

    Update to Python3.

--- tools/install_darwin.sh
+++ tools/install_darwin.sh
@@ -5,52 +5,50 @@ set -e
 # store location of this script
 TOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# store Python version
+PYTHON_VERSION=3.7.3
+
 # clean env
 module purge
 module load openmpi/2.1.3-gcc_6.4.0
 module load anaconda/Anaconda3.2019.03
 source deactivate
-conda env remove -y -n spotlight
+conda env remove --yes --name spotlight
 
 # create Anaconda env
-conda create -y -n spotlight  python=2.7
+conda create --yes --name spotlight python=${PYTHON_VERSION}
 source activate spotlight
 
-# install Python packages
-pip install --upgrade pip
-pip install "matplotlib==2.0.0"
-pip install "numpy==1.13.1"
-pip install "scipy==1.0.0"
-pip install "dill==0.2.8.1"
-pip install "klepto==0.1.5.2"
-pip install "sympy==1.1.1"
-
-# install mpi4py
-mkdir -p ${CONDA_PREFIX}/src && cd ${CONDA_PREFIX}/src
-git clone -b 3.0.0 https://github.com/mpi4py/mpi4py.git
-cd mpi4py
-python setup.py install
-
-# install mystic
-mkdir -p ${CONDA_PREFIX}/src && cd ${CONDA_PREFIX}/src
-git clone https://github.com/uqfoundation/mystic.git
-cd mystic
-python setup.py install
-
-# install gsas
+# install GSAS which requires Python 2.7 for installation scripts
+conda install --yes python==2.7.16
 mkdir -p ${CONDA_PREFIX}/gsas && cd ${CONDA_PREFIX}/gsas
 curl https://subversion.xray.aps.anl.gov/trac/EXPGUI/browser/gsas/linux/dist/bootstrap.py?format=txt > bootstrap.py
 echo $'proxyout.lanl.gov\n8080' >> proxy.txt
 python bootstrap.py < proxy.txt
 rm proxy.txt
+conda install --yes python==${PYTHON_VERSION}
 
-# install gsaslanguage scripts
+# install gsaslanguage scripts and fix some issues
 cd ${CONDA_PREFIX}/gsas
 git clone https://github.com/Svennito/gsaslanguage.git scripts
+cd scripts
+git reset --hard 8f09750
 chmod +x ${CONDA_PREFIX}/gsas/scripts/gsas_get_current_wtfrac_esd
+OS_NAME=`uname -a`
+if [[ ${OS_NAME} =~ "Darwin" ]]; then
+    sed -i '' '42s/^/#/' ${CONDA_PREFIX}/gsas/scripts/gsas_vary_profile_parameters
+else
+    sed '42s/^/#/' ${CONDA_PREFIX}/gsas/scripts/gsas_vary_profile_parameters
+fi
+
+# install required packages
+conda install --yes pkg-config==0.29.2
+
+# install optional packages
+conda install --yes --channel conda-forge imagemagick
 
 # install patchelf
-cd ${CONDA_PREFIX}/src
+mkdir -p ${CONDA_PREFIX}/src && cd ${CONDA_PREFIX}/src
 git clone -b 0.10 https://github.com/NixOS/patchelf.git
 cd patchelf
 ./bootstrap.sh
@@ -66,11 +64,25 @@ for EXE in `ls ${CONDA_PREFIX}/gsas/exe/*`; do
 patchelf --set-interpreter ${PWD}/lib/ld-linux.so.2 ${EXE}
 done
 
-# install spotlight
+# install OpenMPI
+cd ${CONDA_PREFIX}/src
+wget https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.3.tar.gz
+tar -xvf openmpi-2.1.3.tar.gz
+cd openmpi-2.1.3
+CFLAGS=-O3 \
+CXXFLAGS=-O3 \
+./configure --prefix=${CONDA_PREFIX}
+make -j $(getconf _NPROCESSORS_ONLN) install
+
+# install Python packages
+python -m pip install --upgrade pip
+python -m pip install --requirement ${TOOLS_DIR}/../requirements.txt
+
+# install Spotlight
 cd ${TOOLS_DIR}/..
 python setup.py install
 
-# update virtual environment
+# append env activation script
 mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
 echo module load openmpi/2.1.3-gcc_6.4.0 > ${CONDA_PREFIX}/etc/conda/activate.d/post.sh
 echo export PGPLOT_FONT=\$\{CONDA_PREFIX\}/pgl/grfont.dat >> ${CONDA_PREFIX}/etc/conda/activate.d/post.sh
