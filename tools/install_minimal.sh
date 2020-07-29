ce417418cff0f39c97760ae29aecf90093a54b30
Christopher Michael Biwer <cmbiwer@lanl.gov>
2020-07-29 09:35:34 -0600

    MPI not required.

--- tools/install_minimal.sh
+++ tools/install_minimal.sh
@@ -7,6 +7,5 @@ conda create --yes --name spotlight python=3.7.3
 conda activate spotlight
 
 # install MPI with Python bindings, Mystic, and Spotlight
-conda install --yes mpi4py==3.0.3
 python -m pip install klepto==0.1.7 https://github.com/uqfoundation/mystic/archive/5b73d70.tar.gz
 python -m pip install https://github.com/lanl/spotlight/archive/master.tar.gz
