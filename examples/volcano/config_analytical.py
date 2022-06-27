f3d4fc0e8873e9b8b13cdff4e91abd39f990b956
Christopher M. Biwer <cmbiwer@lanl.gov>
2022-06-27 15:24:44 -0600

    Simplify Volcano Python file. (#30)

--- examples/volcano/config_analytical.py
+++ examples/volcano/config_analytical.py
@@ -5,21 +5,6 @@ import numpy
 from scipy import stats
 from spotlight import plan
 
-def gaussian(x, y):
-    """ Creates a Gaussian.
-    """
-    var = stats.multivariate_normal(mean=[0, 0], cov=[[0.5, 0],[0, 0.5]])
-    return -50.0 * var.pdf([x, y])
-
-def analytical_function(*p):
-    """ The analytical response function to evaluate.
-    """
-    r = numpy.sqrt(p[0]**2 + p[1]**2)
-    mu, sigma = 5.0, 1.0
-    return 25.0 * (numpy.exp(-r / 35.0) + 1.0 /
-                   (sigma * numpy.sqrt(2.0 * numpy.pi)) *
-                    numpy.exp(-0.5 * ((r - mu) / sigma) ** 2)) + gaussian(*p)
-
 class Plan(plan.BasePlan):
 
     # required to have solution_file, state_file, num_solvers, and tag
@@ -48,12 +33,31 @@ class Plan(plan.BasePlan):
     }
 
     def initialize(self):
+        """ Executed once at the beginning to set up the problem.
+        """
         pass
 
     def compute(self):
+        """ Executed for each set of drawn parameters in the optimization search.
+        """
 
-        sign = self.surface.sign if hasattr(self, "surface") else 1.0
-        stat = analytical_function(self.get("x"), self.get("y"))
-        stat *= sign
+        # get the x and y values from Mystic
+        x, y = self.get("x"), self.get("y")
+
+        # get value at Gaussian function x and y
+        var = stats.multivariate_normal(mean=[0, 0], cov=[[0.5, 0],[0, 0.5]])
+        gauss = -50.0 * var.pdf([x, y])
+
+        # get value at volcano function x and y
+        r = numpy.sqrt(x**2 + y**2)
+        mu, sigma = 5.0, 1.0
+        stat = 25.0 * (numpy.exp(-r / 35.0) + 1.0 /
+                       (sigma * numpy.sqrt(2.0 * numpy.pi)) *
+                        numpy.exp(-0.5 * ((r - mu) / sigma) ** 2)) + gauss
+
+        # whether to flip sign of function
+        # a positive lets you search for minimum
+        # a negative lets you search for maximum
+        stat *= self.surface.sign if hasattr(self, "surface") else 1.0
 
         return stat
