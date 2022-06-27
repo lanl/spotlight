41f1d728648d7177873b6e6ae546d2f3aa033adc
Christopher M. Biwer <cmbiwer@lanl.gov>
2022-06-27 15:14:38 -0600

    Indent script.

--- examples/volcano/run_spotlight.sh
+++ examples/volcano/run_spotlight.sh
@@ -13,45 +13,40 @@ fi
 # loop over number of local optimizations per thread
 for N in 2 8 32; do
 
-# make a temporary directory for analysis
-mkdir -p tmp_map_${N}
-cd tmp_map_${N}
-
-# store hostname
-echo `hostname` > host.txt
-
-# random seed
-SEED=321
-
-# run optimization search in parallel
-# profile the execution
-${EXE} --config-files ../config_analytical.py \
-       --config-overrides \
-           configuration:seed:${SEED} \
-           configuration:tag:${SEED} \
-           configuration:num_solvers:${N} \
-       --tmp-dir tmp
-
-# random seed
-SEED=123
-
-# run optimization search in parallel
-# profile the execution
-${EXE} --config-files ../config_analytical.py \
-       --config-overrides \
-           configuration:seed:${SEED} \
-           configuration:tag:${SEED} \
-           configuration:num_solvers:${N} \
-           surface:sign:-1 \
-       --tmp-dir tmp
-
-# plot
-python ../spotlight_interpolate.py \
-    --input-file solution.db \
-    --output-file ../map_${N}.png \
-    --abs
-
-# back to original directory
-cd ..
+    # make a temporary directory for analysis
+    mkdir -p tmp_map_${N}
+    cd tmp_map_${N}
+    
+    # random seed
+    SEED=321
+    
+    # run optimization search
+    ${EXE} --config-files ../config_analytical.py \
+           --config-overrides \
+               configuration:seed:${SEED} \
+               configuration:tag:${SEED} \
+               configuration:num_solvers:${N} \
+           --tmp-dir tmp
+    
+    # random seed
+    SEED=123
+    
+    # run optimization search
+    ${EXE} --config-files ../config_analytical.py \
+           --config-overrides \
+               configuration:seed:${SEED} \
+               configuration:tag:${SEED} \
+               configuration:num_solvers:${N} \
+               surface:sign:-1 \
+           --tmp-dir tmp
+    
+    # plot
+    python ../spotlight_interpolate.py \
+        --input-file solution.db \
+        --output-file ../map_${N}.png \
+        --abs
+    
+    # back to original directory
+    cd ..
 
 done
