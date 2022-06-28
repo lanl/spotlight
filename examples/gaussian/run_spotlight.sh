3bd654095a72920cf316fb72d3f976fad1ca536b
Christopher M. Biwer <cmbiwer@lanl.gov>
2022-06-28 10:25:15 -0600

    Remove comments. Do not use MPI in Gaussian example. (#35)

--- examples/gaussian/run_spotlight.sh
+++ examples/gaussian/run_spotlight.sh
@@ -6,12 +6,8 @@ set -e
 mkdir -p tmp
 cd tmp
 
-# set the number of threads to use for parallel regions
-export OMP_NUM_THREADS=1
-
 # run optimization search in parallel
-# profile the execution
-mpirun spotlight_minimize --config-files ../config_analytical.py
+spotlight_minimize --config-files ../config_analytical.py
 
 # print best result
 spotlight_inspect --input-file solution.db
