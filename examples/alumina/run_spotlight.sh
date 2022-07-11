e150a79d86aa9cf277e6be94b891863053dc592f
Christopher M. Biwer <cmbiwer@lanl.gov>
2022-07-11 16:29:36 -0600

    Use Python configuration file for alumina example (#36)
    * Change val to value.
    
    * Add a generic container class.
    
    * Use Python config in alumina example.
    
    * Rename config file.
    
    * Docstring for container.
    
    * Remove the phase overrides.
    
    * Remove profiling.

--- examples/alumina/run_spotlight.sh
+++ examples/alumina/run_spotlight.sh
@@ -10,11 +10,18 @@ set -e
 SEED=${1}
 SEED=${SEED:=123}
 
+# if MPI is installed then use it
+if [ -x "$(command -v mpirun)" ]; then
+    export OMP_NUM_THREADS=1
+    EXE="mpirun --oversubscribe -n 4 python -m cProfile -o analytical.pstat `which spotlight_minimize`"
+else
+    EXE=`which spotlight_minimize`
+fi
+
 # make a temporary directory for analysis
 mkdir -p tmp_spotlight_${SEED}
 cd tmp_spotlight_${SEED}
 cp ../al2o3001.gsa ../alumina.cif ../bt1demo.ins .
-cp ../config_base.ini ../config_alumina.ini ../plan_alumina.py .
 
 # store hostname
 echo `hostname` > host.txt
@@ -22,22 +29,12 @@ echo `hostname` > host.txt
 # convert from CIF to EXP
 gsas_convert_cif alumina.cif alumina.exp
 
-# set the number of threads to use for parallel regions
-export OMP_NUM_THREADS=1
-
-# run optimization search in parallel
-# profile the execution
-mpirun \
-    python -m cProfile -o alumina.pstat `which spotlight_minimize` \
-    --config-files \
-        config_base.ini \
-        config_alumina.ini \
-    --config-overrides \
-        configuration:seed:${SEED} \
-        configuration:tag:${SEED} \
-        phases-0:phase_file:alumina.exp \
-        phases-0:phase_number:1 \
-    --tmp-dir tmp
+# run optimization search
+${EXE} --config-files \
+           ../config_alumina.py \
+       --config-overrides \
+           configuration:seed:${SEED} \
+           configuration:tag:${SEED}
 
 # setup GSAS for global minima from optimization search
 spotlight_gsas_setup \
@@ -50,28 +47,17 @@ spotlight_gsas_setup \
 # so check if it is installed first
 cd tmp_minima
 for IDX in 1; do
-gsas_write_csv ${IDX} TRIAL hist_${IDX}.txt
-spotlight_plot_profile \
-    --input-file hist_${IDX}.txt \
-    --profile-file profile_${IDX}.png \
-    --residual-file residual_${IDX}.png \
-    --reflections-file reflections_${IDX}.png \
-    --phase-labels Alumina
-if [ -x "$(command -v convert)" ]; then
-convert -coalesce profile_${IDX}.png reflections_${IDX}.png residual_${IDX}.png alumina_${IDX}.pdf
-fi
+    gsas_write_csv ${IDX} TRIAL hist_${IDX}.txt
+    spotlight_plot_profile \
+        --input-file hist_${IDX}.txt \
+        --profile-file profile_${IDX}.png \
+        --residual-file residual_${IDX}.png \
+        --reflections-file reflections_${IDX}.png \
+        --phase-labels Alumina
+    if [ -x "$(command -v convert)" ]; then
+    convert -coalesce profile_${IDX}.png reflections_${IDX}.png residual_${IDX}.png alumina_${IDX}.pdf
+    fi
 done
 mv *.png *.pdf ..
 cd ..
 
-# plot profiling information
-# requires gprof2dot which is not an explicit dependency of Spotlight
-# so check if it is installed first
-if [ -x "$(command -v gprof2dot)" ]; then
-for IDX in $(seq 0 $((`getconf _NPROCESSORS_ONLN` - 1))); do
-gprof2dot -f pstats tmp_${IDX}/alumina.pstat | dot -Tpdf -o pstat_${IDX}.pdf
-done
-fi
-
-## make PDF
-#gsas_done
---
+++ spotlight/container.py
@@ -0,0 +1,23 @@
+""" This module contains classes that act as generic containers holding the attributes
+given in its constructor.
+"""
+
+class Container(object):
+    """ This class contains information passed in the constructor. For example,
+    information about the detector or instrument. Any keywords given to the
+    constructor will be added as attributes.
+    """
+
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            if not isinstance(val, str):
+                setattr(self, key, val)
+            elif val.isdigit():
+                setattr(self, key, int(val))
+            else:
+                try:
+                    val = float(val)
+                    setattr(self, key, val)
+                except ValueError:
+                    setattr(self, key, val)
+
