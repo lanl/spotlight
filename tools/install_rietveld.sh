e6be764da88ed3252bf0ef10dab2ee46137fee0b
Christopher M. Biwer <cmbiwer@lanl.gov>
2022-08-01 09:35:54 -0600

    Add Rietveld environment.

---
+++ tools/install_rietveld.sh
@@ -0,0 +1,18 @@
+# /bin/bash
+
+set -e 
+
+# install and load Anaconda env
+conda env create -f environment_reitveld.yaml
+conda activate spotlight-rietveld
+
+# build GSAS-II
+cd ${CONDA_PREFIX}/GSASII
+./bootstrap.py
+cd fsource
+scons
+
+# append env activation script
+mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
+echo export PYTHONPATH=\$\{PYTHONPATH\}:\$\{CONDA_PREFIX\}/GSASII:\$\{CONDA_PREFIX\}/GSASII/fsource >> ${CONDA_PREFIX}/etc/conda/activate.d/post.sh
+echo export MAUD=/opt/Maud_2p99 >> ${CONDA_PREFIX}/etc/conda/activate.d/post.sh
