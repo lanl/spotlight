e8544f13851f74f8e7b87e7608c10e877206c4d7
Christopher M. Biwer <cmbiwer@lanl.gov>
2022-08-01 13:25:06 -0600

    Add plots.

--- docs/_static/tutorial_mystic_cost.py
+++ docs/_static/tutorial_mystic_cost.py
@@ -1,18 +1,18 @@
-
 import matplotlib.pyplot as plt
+import numpy
+import os
 from scipy import stats
-from spotlight import solver
 
-def func(p):
+def func(*p):
     """ Executed for each set of drawn parameters in the optimization search.
     """
 
     # get the x and y values from Mystic
-    x, y = p[0], p[1]
+    x = p[0]
 
     # get value at 2-D Gaussian function x and y
-    var = stats.multivariate_normal(mean=[0, 0], cov=[[0.5, 0],[0, 0.5]])
-    stat = -50.0 * var.pdf([x, y])
+    var = stats.multivariate_normal(mean=[4.25], cov=[[1]])
+    stat = -50.0 * var.pdf([x])
 
     # set sign of function
     # a positive lets you search for minimum
@@ -21,11 +21,7 @@ def func(p):
 
     return stat
 
-s = solver.Solver([-9.5, -9.5], [9.5, 9.5],
-                  local_solver="powell",
-                  sampling_method="uniform")
+x = numpy.linspace(-9.5, 9.5, 100)
+plt.plot(x, [func(i) for i in x])
 
-s.solve(func)
-
-print(s.solution)
-
+plt.savefig(os.path.join(os.path.dirname(__file__), "tmp_cost.png"))
--- docs/tutorial_mystic.rst
+++ docs/tutorial_mystic.rst
@@ -15,15 +15,24 @@ Designing a cost function...
 
 .. literalinclude:: _static/tutorial_mystic_cost_function.py
 
+.. image:: _static/tmp_cost.png
+
 Optimizers
 ----------
 
 Text...
 
-.. literalinclude:: _static/tutorial_spotlight_solve.py
+.. literalinclude:: _static/tutorial_mystic_solve.py
+
+.. image:: _static/tmp_single.png
 
 Termination
 -----------
+
+Text...
+
+Multi-dimensional
+-----------------
 
 Text...
 
