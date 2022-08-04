4ca17653af07ce4d0dbfa9a3fc70ef4efce25c11
Christopher M. Biwer <cmbiwer@lanl.gov>
2022-08-04 16:21:03 -0600

    Update solver.

--- spotlight/sampling.py
+++ spotlight/sampling.py
@@ -10,12 +10,15 @@ from spotlight import container
 
 class SamplingBase(container.Container):
 
+    def __init__(self, lower_bounds, upper_bounds):
+        super().__init__(lower_bounds=lower_bounds, upper_bounds=upper_bounds)
+
     def sample(self, *args, **kwargs):
         raise NotImplementedError
 
 class UniformSampling(SamplingBase):
 
-    def sample(self, lower_bounds, upper_bounds):
+    def sample(self):
         """ Returns a new, single point via uniform distribution.
     
         Parameters
@@ -30,17 +33,20 @@ class UniformSampling(SamplingBase):
         pts : numpy.array
             An array with new point.
         """
-        ndim = len(lower_bounds)
+        ndim = len(self.lower_bounds)
         pts = numpy.zeros(ndim)
         for j in range(ndim):
-            lb = lower_bounds[j]
-            ub = upper_bounds[j]
+            lb = self.lower_bounds[j]
+            ub = self.upper_bounds[j]
             pts[j] = numpy.random.uniform(lb, ub)
         return pts
 
 class ToleranceSampling(SamplingBase):
 
-    def sample(self, lower_bounds, upper_bounds, data=None):
+    def __init__(self, lower_bounds, upper_bounds, data=None):
+        super().__init__(lower_bounds=lower_bounds, upper_bounds=upper_bounds, data=data)
+
+    def sample(self):
         """ Returns a new, single point some tolerence away from existing points.
     
         Parameters
@@ -61,44 +67,43 @@ class ToleranceSampling(SamplingBase):
         rtol = None
         dist = None
         n_new_pts = 1
-        data = [] if data == None else data
+        data = [] if self.data == None else self.data
         if len(data) == 0:
-            return uniform(lower_bounds, upper_bounds)
-        pts = math.fillpts(lower_bounds, upper_bounds, n_new_pts, data, rtol, dist)
+            return uniform(self.lower_bounds, self.upper_bounds)
+        pts = math.fillpts(self.lower_bounds, self.upper_bounds, n_new_pts, data, rtol, dist)
         return pts[0]
 
 class LinearSampling(SamplingBase):
 
-    def sample(self, lower_bounds, upper_bounds, step=0, nsteps=1):
+    def __init__(self, lower_bounds, upper_bounds, step=0, nsteps=1):
+        super().__init__(lower_bounds=lower_bounds, upper_bounds=upper_bounds,
+                         step=step, nsteps=nsteps)
+
+    def sample(self, step=None):
         """ Returns a new, single point linearlly-spaced ``i`` from the lower
         boundary.
     
         Parameters
         ----------
-        lower_bounds : list
-            List of ``float`` that are lower bounds. List is indexed by parameter.
-        upper_bounds : list
-            List of ``float`` that are upper bounds. List is indexed by parameter.
         step : int
             Index to select.
-        nsteps : int
-            Total number of steps.
     
         Returns
         -------
         pts : numpy.array
             An array with new point.
         """
-        ndim = len(lower_bounds)
+        step = self.step if step is None else step
+        ndim = len(self.lower_bounds)
         pts = numpy.zeros(ndim)
         for j in range(ndim):
-            step_size = (upper_bounds[j] - lower_bounds[j]) / (nsteps)
-            pts[j] = step * step_size + lower_bounds[j] + 0.5 * step_size
+            step_size = (self.upper_bounds[j] - self.lower_bounds[j]) / (self.nsteps)
+            pts[j] = step * step_size + self.lower_bounds[j] + 0.5 * step_size
         return pts
 
 class MidpointSampling(SamplingBase):
 
-    def sample(self, lower_bounds, upper_bounds):
+    def sample(self):
         """ Returns a new, single point at the center.
     
         Parameters
@@ -113,11 +118,11 @@ class MidpointSampling(SamplingBase):
         pts : numpy.array
             An array with new point.
         """
-        ndim = len(lower_bounds)
+        ndim = len(self.lower_bounds)
         pts = numpy.zeros(ndim)
         for j in range(ndim):
-            lb = lower_bounds[j]
-            ub = upper_bounds[j]
+            lb = self.lower_bounds[j]
+            ub = self.upper_bounds[j]
             pts[j] = (ub - lb) / 2.0 + lb
         return pts
 
