b5a21597496eafc03f6a9679933609f21cca9179
Christopher Biwer <cmbiwer@lanl.gov>
2022-08-14 16:50:27 -0600

    State that spotlight.bridge is actually from mystic. Do not take credit for the code.

---
+++ spotlight/bridge/__init__.py
@@ -0,0 +1,4 @@
+""" This subpackage contains modules for creating surrogate models with Mystic.
+These modules were only available in the ``examples3/`` dir in Mystic, and therefore,
+they are copied here until they are available in a Mystic release.
+"""
