2f4c17b69eb4db25b537918c2ca77be664d42449
Christopher M. Biwer <cmbiwer@lanl.gov>
2022-08-17 10:53:48 -0600

    Update tutorial with more descriptive text (#67)
    * Change to use MAUD_PATH.
    
    * Add comment about post.sh snippet.
    
    * Update output for long MILK opt.
    
    * Add scikitlearn link.
    
    * Example for MILK data paths in different dirs.
    
    * More text.
    
    * Whitespace.

--- tools/install_rietveld.sh
+++ tools/install_rietveld.sh
@@ -12,7 +12,14 @@ cd ${CONDA_PREFIX}/GSASII
 cd fsource
 scons
 
-# append env activation script
-mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
-echo export PYTHONPATH=\$\{PYTHONPATH\}:\$\{CONDA_PREFIX\}/GSASII:\$\{CONDA_PREFIX\}/GSASII/fsource >> ${CONDA_PREFIX}/etc/conda/activate.d/post.sh
-echo export MAUD=/opt/Maud_2p99 >> ${CONDA_PREFIX}/etc/conda/activate.d/post.sh
+# add GSAS-II to Python
+export PYTHONPATH=${PYTHONPATH}:${CONDA_PREFIX}/GSASII:${CONDA_PREFIX}/GSASII/fsource
+
+# set path to MAUD for MILK
+export MAUD_PATH=/path/to/my/Maud.app
+
+## you can also append to the Anaconda env
+## activation script to set env variables when loaded
+#mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
+#echo export PYTHONPATH=\$\{PYTHONPATH\}:\$\{CONDA_PREFIX\}/GSASII:\$\{CONDA_PREFIX\}/GSASII/fsource >> ${CONDA_PREFIX}/etc/conda/activate.d/post.sh
+#echo export MAUD_PATH=/path/to/my/Maud.app >> ${CONDA_PREFIX}/etc/conda/activate.d/post.sh
